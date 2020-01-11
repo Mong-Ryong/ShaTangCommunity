33ef2a683c3173fd4e76725dce8b1c3589956319
Mong-Ryong <mong-ryong@qq.com>
2020-01-11 09:05:15 +0800

    页面封装

--- src/main/java/com/ml/community/controller/IndexController.java
+++ src/main/java/com/ml/community/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.ml.community.controller;
 
+import com.ml.community.dto.PaginationDTO;
 import com.ml.community.dto.QuestionDTO;
 import com.ml.community.mapper.QuestionMapper;
 import com.ml.community.mapper.UserMapper;
@@ -27,7 +28,9 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(HttpServletRequest request,
-                        Model model) {
+                        Model model,
+                        @RequestParam(name = "page",defaultValue = "1")Integer page,
+                        @RequestParam(name = "size",defaultValue = "5")Integer size) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0)
             for (Cookie cookie : cookies) {
@@ -40,8 +43,8 @@ public class IndexController {
                     break;
                 }
             }
-        List<QuestionDTO> questionList = questionService.list();
-        model.addAttribute("questions", questionList);
+        PaginationDTO pagination = questionService.list(page,size);
+        model.addAttribute("pagination", pagination);
         return "index";
     }
 }
--- src/main/java/com/ml/community/service/QuestionService.java
+++ src/main/java/com/ml/community/service/QuestionService.java
@@ -1,5 +1,6 @@
 package com.ml.community.service;
 
+import com.ml.community.dto.PaginationDTO;
 import com.ml.community.dto.QuestionDTO;
 import com.ml.community.mapper.QuestionMapper;
 import com.ml.community.mapper.UserMapper;
@@ -21,17 +22,31 @@ public class QuestionService {
     @Autowired
     private UserMapper userMapper;
 
-    public List<QuestionDTO> list() {
-        List<Question> questions = questionMapper.list();
+    public PaginationDTO list(Integer page, Integer size) {
+        Integer totalCount = questionMapper.count();
+        PaginationDTO paginationDTO = new PaginationDTO();
+
+        paginationDTO.setPagination(totalCount, page, size);
+        if (page < 1) {
+            page = 1;
+        }
+        if (page > paginationDTO.getTotalPage()) {
+            page = paginationDTO.getTotalPage();
+        }
+
+        Integer offset = size * (page - 1);
+
+        List<Question> questions = questionMapper.list(offset, size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
         for (Question question : questions) {
-            User user =userMapper.findById(question.getCreator());
+            User user = userMapper.findById(question.getCreator());
             QuestionDTO questionDTO = new QuestionDTO();
             //快速把question中的属性拷贝到questionDTO中
-            BeanUtils.copyProperties(question,questionDTO);
+            BeanUtils.copyProperties(question, questionDTO);
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
         }
-        return questionDTOList;
+        paginationDTO.setQuestions(questionDTOList);
+        return paginationDTO;
     }
 }
