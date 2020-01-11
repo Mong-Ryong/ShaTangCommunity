d73af1ea5c8d0d02e266745fb0f57d50bd0042ae
Mong-Ryong <mong-ryong@qq.com>
2020-01-11 11:02:26 +0800

    my questions

--- src/main/java/com/ml/community/controller/IndexController.java
+++ src/main/java/com/ml/community/controller/IndexController.java
@@ -18,6 +18,9 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
+/**
+ *首页
+ */
 @Controller
 public class IndexController {
     @Autowired
--- src/main/java/com/ml/community/controller/PublishController.java
+++ src/main/java/com/ml/community/controller/PublishController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * 问题发布页面
+ */
 @Controller
 public class PublishController {
 
